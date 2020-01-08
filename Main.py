6e4049c625cff0cec761ad16e33a81f514a73587
Joris Quist <Joris Quist>
2020-01-08 14:28:34 +0100

    Working buoyancy

--- Main.py
+++ Main.py
@@ -11,25 +11,34 @@ cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 boxId = p.loadURDF("r2d2.urdf", cubeStartPos, cubeStartOrientation)
 
-colSphereId = p.createVisualShape(p.GEOM_SPHERE, radius=0.1)
+colSphereId = p.createVisualShape(p.GEOM_SPHERE, radius=0.05, rgbaColor=[1, 0, 0, 0.4])
 
 p.changeDynamics(boxId, -1, linearDamping=0.9, angularDamping=0.9)
 
 # p.setTimeStep(1./1440.)
 
 buoyancyForceSlider = p.addUserDebugParameter("buoyancyForce", 0, 1000, 650)
+buoyancyXSlider = p.addUserDebugParameter("buoyancy x", -1, 1, 0)
+buoyancyYSlider = p.addUserDebugParameter("buoyancy y", -1, 1, 0)
+buoyancyZSlider = p.addUserDebugParameter("buoyancy z", -1, 1, 0)
 
-sphereUid = -1
+sphereId = p.createMultiBody(0, -1, colSphereId, [0, 0, 0], useMaximalCoordinates=0)
 
-for i in range(100000):
+while(True):
     cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+    spherePos, sphereOrn = p.getBasePositionAndOrientation(sphereId)
 
     buoyancy = p.readUserDebugParameter(buoyancyForceSlider)
 
-    buoyancyForcePos = np.reshape(np.array(p.getMatrixFromQuaternion(cubeOrn)), (3, 3)).dot(np.array([0, 0, -0.5])) \
-                       + cubePos
+    buoyancyX = p.readUserDebugParameter(buoyancyXSlider)
+    buoyancyY = p.readUserDebugParameter(buoyancyYSlider)
+    buoyancyZ = p.readUserDebugParameter(buoyancyZSlider)
 
-    sphereUid = p.createMultiBody(0, -1, colSphereId, buoyancyForcePos, useMaximalCoordinates=0)
+    buoyancyForcePos = np.reshape(np.array(p.getMatrixFromQuaternion(cubeOrn)), (3, 3)).dot(np.array([buoyancyX, buoyancyY, buoyancyZ])) \
+        + cubePos
+
+    # sphereUid = p.createMultiBody(0, -1, colSphereId, buoyancyForcePos, useMaximalCoordinates=0)
+    p.resetBasePositionAndOrientation(sphereId, np.array(buoyancyForcePos), cubeOrn)
 
     p.applyExternalForce(objectUniqueId=boxId, linkIndex=-1,
                          forceObj=[0, 0, buoyancy], posObj=np.array(buoyancyForcePos), flags=p.WORLD_FRAME)
