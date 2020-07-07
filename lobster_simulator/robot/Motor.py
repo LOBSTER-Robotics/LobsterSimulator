68014aedc05f46b8a9b37aa3a3c51a91e0010422
Joris Quist <Joris Quist>
2020-07-07 15:26:17 +0200

    Added a maximum rpm to the motor

--- lobster_simulator/robot/Motor.py
+++ lobster_simulator/robot/Motor.py
@@ -5,7 +5,7 @@ import pybullet as p
 
 class Motor:
 
-    def __init__(self, pybullet_id, name, position, direction, rpm_to_thrust, thrust_to_rpm, max_rpm_change_per_second):
+    def __init__(self, pybullet_id, name, position, direction, rpm_to_thrust, thrust_to_rpm, max_rpm_change_per_second, max_rpm):
         self.name = name
         self.pybullet_id = pybullet_id
         self.position = position
@@ -15,6 +15,7 @@ class Motor:
         self._thrust_to_rpm = thrust_to_rpm
 
         self.max_rpm_change_per_second = max_rpm_change_per_second
+        self.max_rpm = max_rpm
 
         self.rpm = 0
         self.desired_rpm = 0
@@ -24,13 +25,16 @@ class Motor:
         rpm_to_thrust = lambda rpm: 3.92 / 1000 * rpm + 3.9 / 10000000 * rpm * rpm + 7.55 / 10000000000 * rpm * rpm * rpm
         thrust_to_rpm = lambda x: -172.185 - 5.05393 * pow(261.54 * math.sqrt(3.84767 * math.pow(10, 8) * math.pow(x, 2) + 5.13478 * math.pow(10, 8) * x + 4.48941 * math.pow(10,9)) - 5.13023 * math.pow(10, 6) * x - 3.42319 * math.pow(10, 6), (1 / 3)) + 336577. / math.pow(261.54 * math.sqrt(3.84767 * pow(10, 8) * math.pow(x, 2) + 5.13478 * math.pow(10, 8) * x + 4.48941 * math.pow(10,9)) - 5.13023 * math.pow(10, 6) * x - 3.42319 * math.pow(10, 6), (1 / 3))
 
-        return Motor(pybullet_id, name, position, direction, rpm_to_thrust, thrust_to_rpm, max_rpm_change_per_second=40000)
+        return Motor(pybullet_id, name, position, direction, rpm_to_thrust, thrust_to_rpm, max_rpm_change_per_second=40000, max_rpm=4000)
 
     def set_desired_rpm(self, desired_rpm):
+        desired_rpm = min(desired_rpm, self.max_rpm)
+        desired_rpm = max(desired_rpm, -self.max_rpm)
+
         self.desired_rpm = desired_rpm
 
     def set_desired_thrust(self, desired_thrust):
-        self.desired_rpm = self._thrust_to_rpm(desired_thrust)
+        self.set_desired_rpm(self._thrust_to_rpm(desired_thrust))
 
     def get_thrust(self):
         return self.rpm_to_thrust(self.rpm)
