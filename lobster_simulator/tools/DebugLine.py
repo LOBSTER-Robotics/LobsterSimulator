5c121bd63157030ed8cd419410ce9789f43677d6
Gijs Koning <gijs-koning@live.nl>
2020-07-16 19:02:34 +0200

    Update debuglines less frequently such that GUI will a lot faster

--- lobster_simulator/tools/DebugLine.py
+++ lobster_simulator/tools/DebugLine.py
@@ -1,27 +1,37 @@
+import pybullet as p
+import time
+
 from lobster_simulator.tools import Translation
-from lobster_simulator.tools.PybulletAPI import PybulletAPI
 
 
 class DebugLine:
+    MAX_UPDATE_FREQUENCY = 0.05
 
     def __init__(self, from_location, to_location, width=5, color=None):
         if color is None:
             color = [1, 0, 0]
 
-        self._width = width
-        self._color = color
-        self._id = PybulletAPI.addUserDebugLine(lineFromXYZ=from_location,
-                                                lineToXYZ=to_location,
-                                                lineWidth=width,
-                                                lineColorRGB=color)
+        self.width = width
+        self.color = color
+        self.id = p.addUserDebugLine(lineFromXYZ=from_location,
+                                     lineToXYZ=to_location,
+                                     lineWidth=width,
+                                     lineColorRGB=color)
+        self._latest_update_time = time.time()
 
-    def update(self, from_location, to_location, frame_id=None):
+    def update(self, from_location, to_location, frame_id=None) -> None:
+        if not self.can_update():
+            return
         if frame_id:
             from_location = Translation.vec3_local_to_world_id(frame_id, from_location)
             to_location = Translation.vec3_local_to_world_id(frame_id, to_location)
 
-        self._id = PybulletAPI.addUserDebugLine(lineFromXYZ=from_location,
-                                                lineToXYZ=to_location,
-                                                lineWidth=self._width,
-                                                lineColorRGB=self._color,
-                                                replaceItemUniqueId=self._id)
+        self.id = p.addUserDebugLine(lineFromXYZ=from_location,
+                                     lineToXYZ=to_location,
+                                     lineWidth=self.width,
+                                     lineColorRGB=self.color,
+                                     replaceItemUniqueId=self.id)
+        self._latest_update_time = time.time()
+
+    def can_update(self) -> bool:
+        return time.time() - self._latest_update_time > MAX_UPDATE_FREQUENCY
