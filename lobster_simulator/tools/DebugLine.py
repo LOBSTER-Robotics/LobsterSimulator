beceec9544b680f768bbc579d644e47f083f116c
Gijs Koning <gijs-koning@live.nl>
2020-07-17 09:44:34 +0200

    Add tests for debug line

--- lobster_simulator/tools/DebugLine.py
+++ lobster_simulator/tools/DebugLine.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pybullet as p
 import time
 
@@ -5,33 +7,43 @@ from lobster_simulator.tools import Translation
 
 
 class DebugLine:
-    MAX_UPDATE_FREQUENCY = 0.05
+    """
+    Class used to create a debug line in the GUI.
+    """
+    _MAX_UPDATE_FREQUENCY = 0.05
 
-    def __init__(self, from_location, to_location, width=5, color=None):
+    def __init__(self, from_location: List[float], to_location: List[float], width: float = 5, color: List[int] = None):
         if color is None:
             color = [1, 0, 0]
 
-        self.width = width
-        self.color = color
-        self.id = p.addUserDebugLine(lineFromXYZ=from_location,
-                                     lineToXYZ=to_location,
-                                     lineWidth=width,
-                                     lineColorRGB=color)
+        self._width = width
+        self._color = color
+        self._id = -1
+        self._id = self._add_debug_line(from_location, to_location)
         self._latest_update_time = time.time()
 
-    def update(self, from_location, to_location, frame_id=None) -> None:
+    def update(self, from_location: List[float], to_location: List[float], frame_id: int = None) -> None:
+        """
+        Update the pose of the debug line.
+        """
         if not self.can_update():
             return
         if frame_id:
             from_location = Translation.vec3_local_to_world_id(frame_id, from_location)
             to_location = Translation.vec3_local_to_world_id(frame_id, to_location)
 
-        self.id = p.addUserDebugLine(lineFromXYZ=from_location,
-                                     lineToXYZ=to_location,
-                                     lineWidth=self.width,
-                                     lineColorRGB=self.color,
-                                     replaceItemUniqueId=self.id)
+        self._id = self._add_debug_line(from_location, to_location)
         self._latest_update_time = time.time()
 
     def can_update(self) -> bool:
-        return time.time() - self._latest_update_time > MAX_UPDATE_FREQUENCY
+        """
+        Checks if time has passed to allow a new debug line to be created.
+        """
+        return time.time() - self._latest_update_time > self._MAX_UPDATE_FREQUENCY
+
+    def _add_debug_line(self, from_location, to_location) -> int:
+        return p.addUserDebugLine(lineFromXYZ=from_location,
+                                  lineToXYZ=to_location,
+                                  lineWidth=self._width,
+                                  lineColorRGB=self._color,
+                                  replaceItemUniqueId=self._id)
