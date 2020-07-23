8b84b9384e73ab5f11efcc238ab91415b13f63bc
Jorisquist <Jorisquist@gmail.com>
2020-07-23 09:56:31 +0200

    Fixed chunk edge gap

--- test.py
+++ test.py
@@ -14,40 +14,32 @@ useTerrainFromPNG = 1
 useDeepLocoCSV = 2
 updateHeightfield = False
 
-heightfieldSource = useProgrammatic
+heightfieldSource = 2
 import random
 
 def get_height(x, y):
     # math.sin(x / 100) * 10 +
     return math.sin(y / 100)
 
+count = 0
 def load_chunk(x, y):
     numHeightfieldRows = 2 ** 8
     numHeightfieldColumns = 2 ** 8
 
-    chunk_x = x * numHeightfieldRows
-    chunk_y = y * numHeightfieldColumns
-
-    print()
-    print(x,y)
-    print()
+    chunk_x = x * (numHeightfieldRows - 1)
+    chunk_y = y * (numHeightfieldColumns - 1)
 
     heightfieldData = [0.0] * numHeightfieldRows * numHeightfieldColumns
     for j in range(numHeightfieldColumns):
         for i in range(numHeightfieldRows):
 
             height = get_height(chunk_x + i, chunk_y + j)
-            # height =
-            # height *=100
-            # print(chunk_x + i, chunk_y + j, height)
+            if i == 0:
+                print(height)
             heightfieldData[i + j*numHeightfieldRows] = height
 
-    middle = heightfieldData[len(heightfieldData)//2 - 2**7]
-    print(middle)
-    # middle = numpy.mean(heightfieldData)
+    middle = (max(heightfieldData) + min(heightfieldData)) / 2
     # middle = heightfieldData[numHeightfieldRows + numHeightfieldColumns//2*numHeightfieldRows]
-
-
 
     terrainShape = p.createCollisionShape(shapeType=p.GEOM_HEIGHTFIELD, meshScale=[.05, .05, 1],
                                           heightfieldTextureScaling=(numHeightfieldRows - 1) / 2,
@@ -56,11 +48,6 @@ def load_chunk(x, y):
 
     terrain = p.createMultiBody(0, terrainShape)
     p.resetBasePositionAndOrientation(terrain, [0.05 * (2**8-1) * x, 0.05 * (2**8-1) * y, middle], [0, 0, 0, 1])
-
-    result = p.rayTest([0.05 * (2**8-1) * x, 0.05 * (2**8-1) * y, -1],
-                       [0.05 * (2**8-1) * x, 0.05 * (2**8-1) * y, 9])
-
-    print(f"{result[0][2]*10 + 1:0.6f}")
 
 
 random.seed(10)
