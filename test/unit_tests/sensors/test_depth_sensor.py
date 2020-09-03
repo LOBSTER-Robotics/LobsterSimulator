db82fae6295dbf78a298fad6f1ef9fa5dc57c9d8
Gijs Koning <gijs-koning@live.nl>
2020-09-03 09:40:00 +0200

    Add pressure test and fix requirements

---
+++ test/unit_tests/sensors/test_depth_sensor.py
@@ -0,0 +1,16 @@
+import unittest
+
+from lobster_simulator.simulator import Simulator
+from lobster_common.vec3 import Vec3
+
+
+class TestDepthSensor(unittest.TestCase):
+
+    def test_pressure(self):
+        simulator = Simulator(4000, gui=False)
+        robot = simulator.create_robot()
+        robot.set_position_and_orientation(position=Vec3((0,0,0)))
+        pressure = robot._depth_sensor.get_pressure()
+
+        # 101.3 = (0 * ((self._water_density * GRAVITY) + self._OFFSET) + self._STANDARD_ATMOSPHERE) / self._KPA_TO_PA
+        self.assertAlmostEqual(101.3, pressure)
