1b13bf785b8d1747cc68a86d2894300d3d20cc84
Jorisquist <Jorisquist@gmail.com>
2020-09-04 09:24:50 +0200

    Uncommented necessary simulation step

--- test/unit_tests/sensors/test_depth_sensor.py
+++ test/unit_tests/sensors/test_depth_sensor.py
@@ -10,7 +10,7 @@ class DepthSensorTest(unittest.TestCase):
         simulator = Simulator(4000, gui=False)
         robot = simulator.create_robot()
         robot.set_position_and_orientation(position=Vec3((0,0,0)))
-        # simulator.do_step()
+        simulator.do_step()
         pressure = robot._depth_sensor.get_pressure()
         # 101.3 = (0 * ((self._water_density * GRAVITY) + self._OFFSET) + self._STANDARD_ATMOSPHERE) / self._KPA_TO_PA
         self.assertAlmostEqual(101.3, pressure)
