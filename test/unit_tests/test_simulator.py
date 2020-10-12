669b417912aeb0ff708da6e7cb0d13d4cfa2b40a
Jorisquist <Jorisquist@gmail.com>
2020-10-12 21:03:02 +0200

    improved test

--- test/unit_tests/test_simulator.py
+++ test/unit_tests/test_simulator.py
@@ -3,9 +3,6 @@ import math
 import unittest
 from unittest.mock import Mock
 
-from lobster_common.vec3 import Vec3
-import numpy as np
-
 from lobster_simulator.common.pybullet_api import PybulletAPI
 from lobster_simulator.simulator import Simulator
 
@@ -16,18 +13,10 @@ class SimulatorTest(unittest.TestCase):
 
         simulator = Simulator(4000, gui=False)
 
-        temp_load = PybulletAPI.loadURDF
-        temp_change_visual_shape = PybulletAPI.changeVisualShape
+        with unittest.mock.patch("lobster_simulator.common.pybullet_api.PybulletAPI.loadURDF"):
+            with unittest.mock.patch(
+                    "lobster_simulator.common.pybullet_api.PybulletAPI.changeVisualShape"):
 
-        PybulletAPI.loadURDF = Mock()
-        PybulletAPI.changeVisualShape = Mock()
-
-        simulator.add_ocean_floor(100)
-
-
-        PybulletAPI.loadURDF.assert_called_once()
-        PybulletAPI.changeVisualShape.assert_called_once()
-
-        # Putting the original functions back, there might be a better way to do this.
-        PybulletAPI.loadURDF = temp_load
-        PybulletAPI.changeVisualShape = temp_change_visual_shape
+                simulator.add_ocean_floor(100)
+                PybulletAPI.loadURDF.assert_called_once()
+                PybulletAPI.changeVisualShape.assert_called_once()
